256e1320eb60b263f71c5b116ac7348130473f71
gulalicelik <gulalicelikmi@gmail.com>
2022-08-07 15:18:54 +0300

    abone istatistikleri

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import './App.css';
 import Home from './Pages/Home/';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import video from './bg-video.mp4';
 
 function App() {
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: 'https://graphql-weather-api.herokuapp.com/',
-  });
+    const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        uri  : 'https://graphql-weather-api.herokuapp.com/',
+    });
 
-  return (
-    <ApolloProvider client={client}>
-      <div className="app">
-        <div className="bg">
-          <video autoPlay muted loop id="bgVideo">
-            <source src={video} type="video/mp4" />
-            Your browser does not support HTML5 video.
-          </video>
-        </div>
+    return (
+        <ApolloProvider client={client}>
+            <div className="app">
+                <div className="bg">
+                    <video autoPlay muted loop id="bgVideo">
+                        <source src={video} type="video/mp4"/>
+                        Your browser does not support HTML5 video.
+                    </video>
+                </div>
 
-        <div className="layout">
-          <Home />
-        </div>
-      </div>
-    </ApolloProvider>
-  );
+                <div className="layout">
+                    <Home/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias beatae distinctio doloribus eaque
+                        enim molestiae officia quis rem suscipit totam? Enim, molestiae, quas. Asperiores cum hic nobis
+                        porro repellendus, voluptatum.</p>
+                </div>
+            </div>
+        </ApolloProvider>
+    );
 }
 
 export default App;
